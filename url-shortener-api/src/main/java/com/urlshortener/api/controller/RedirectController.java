4c3b74a350751b7571f6259b4290c450e9252296
tejaswikg08dev <tejaswikg08dev@gmail.com>
2026-05-16 14:48:05 +0530

    Getting No Endpoint for GET /shortkey/qr fixed here

--- url-shortener-api/src/main/java/com/urlshortener/api/controller/RedirectController.java
+++ url-shortener-api/src/main/java/com/urlshortener/api/controller/RedirectController.java
@@ -1,12 +1,17 @@
 package com.urlshortener.api.controller;
 
+import com.urlshortener.api.service.QrcodeService;
 import com.urlshortener.api.service.UrlService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -17,6 +22,11 @@ public class RedirectController {
 
     private final UrlService urlService;
 
+    private final QrcodeService qrcodeService;
+
+    @Value("${app.base-url}")
+    private String baseUrl;
+
     @GetMapping("/{shortKey}")
     public ResponseEntity<Void> redirect(
             @PathVariable String shortKey,
@@ -25,5 +35,16 @@ public class RedirectController {
 
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(URI.create(longUrl)).build();
+    }
+
+    @GetMapping("/{shortKey}/qr")
+    public ResponseEntity<byte[]> getQrCode(
+            @PathVariable String shortKey,
+            @RequestParam(defaultValue = "250") int size){
+        String shortUrl = baseUrl+ "/" + shortKey;
+        byte[] qrImage = qrcodeService.generateQrcode(shortUrl, size);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(qrImage);
     }
 }
